57f29aa8ab6346eb7a2dc82c3afc4378ddd38de4
Tasos Laskos <tasos.laskos@gmail.com>
2023-05-23 12:15:43 +0300

    SharedHash: Added #on_set & #on_delete

--- lib/peplum/application/services/shared_hash.rb
+++ lib/peplum/application/services/shared_hash.rb
@@ -6,6 +6,9 @@ class SharedHash
 
   def initialize
     @hash = {}
+
+    @on_set_cb    = {}
+    @on_delete_cb = {}
   end
 
   def get( k )
@@ -16,6 +19,7 @@ class SharedHash
     return if @hash[k] == v
 
     @hash[k] = v
+    call_on_set( k, v )
 
     if broadcast
       each_peer do |peer|
@@ -30,6 +34,7 @@ class SharedHash
     return if !@hash.include? k
 
     @hash.delete( k )
+    call_on_delete( k )
 
     if broadcast
       each_peer do |_, peer|
@@ -40,11 +45,39 @@ class SharedHash
     nil
   end
 
+  def on_set( k, &block )
+    (@on_set_cb[k] ||= []) << block
+  end
+
+  def on_delete( k, &block )
+    (@on_delete_cb ||= []) << block
+  end
+
   def to_h
     @hash.dup
   end
 
   private
+
+  def call_on_set( k, v )
+    return if !@on_set_cb[k]
+
+    @on_set_cb[k].each do |cb|
+      cb.call v
+    end
+
+    nil
+  end
+
+  def call_on_delete( k )
+    return if !@on_delete_cb[k]
+
+    @on_delete_cb[k].each do |cb|
+      cb.call
+    end
+
+    nil
+  end
 
   def each_peer( &block )
     Cuboid::Application.application.peers.each( &block )
