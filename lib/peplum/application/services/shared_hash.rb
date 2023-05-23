952d8605c0846ff239aa62bdc1ce56e910177f48
Tasos Laskos <tasos.laskos@gmail.com>
2023-05-23 11:45:37 +0300

    SharedHash: Made sync

--- lib/peplum/application/services/shared_hash.rb
+++ lib/peplum/application/services/shared_hash.rb
@@ -5,58 +5,43 @@ module Services
 class SharedHash
 
   def initialize
-    @hash         = {}
-    @sync_counter = 0
+    @hash = {}
   end
 
   def get( k )
     @hash[k]
   end
 
-  def set( k, v, broadcast = true, &block )
-    if @hash[k] == v
-      block.call if block_given?
-      return
-    end
+  def set( k, v, broadcast = true )
+    return if @hash[k] == v
 
     @hash[k] = v
 
     if broadcast
       each_peer do |peer|
-        @sync_counter += 1
-        peer.shared_hash.set( k, v, false ) { @sync_counter -= 1 }
+        peer.shared_hash.set( k, v, false )
       end
     end
 
-    block.call if block_given?
     nil
   end
 
   def delete( k, broadcast = true )
-    if !@hash.include? k
-      block.call if block_given?
-      return
-    end
+    return if !@hash.include? k
 
     @hash.delete( k )
 
     if broadcast
       each_peer do |_, peer|
-        @sync_counter += 1
-        peer.shared_hash.delete( k, false ) { @sync_counter -= 1 }
+        peer.shared_hash.delete( k, false )
       end
     end
 
-    block.call if block_given?
     nil
   end
 
   def to_h
     @hash.dup
-  end
-
-  def sync
-    sleep 0.1 while @sync_counter != 0
   end
 
   private
